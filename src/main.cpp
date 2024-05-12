32f25b71c399275ce4af040e34ffcf0b41bc711a
Roman Gobrei <roman.gobrei@gmail.com>
2024-05-12 21:35:32 +0300

    Sample request handler added

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <Arduino.h>
 #include "HttpServer.h"
 
-HttpResponse exampleHandler(String input);
+HttpResponse requestHandler(String input);
 
 HttpServer httpServer;
 
 void setup()
 {
+  // Init HTTP server
   UCHAR serverIp[4] = {192, 168, 1, 2};
   UCHAR gateway[4] = {192, 168, 1, 1};
   UCHAR subnetMask[4] = {255, 255, 255, 0};
@@ -16,13 +17,24 @@ void setup()
 
 void loop()
 {
-  httpServer.handleRequest(&exampleHandler);
+  httpServer.handleRequest(&requestHandler);
 }
 
-HttpResponse exampleHandler(String input)
+HttpResponse requestHandler(String request)
 {
+  // Process the request - GET, POST, etc.
+  // if (GET) - do GET stuff, etc
+
+  // Set headers
+  std::map<String, String> headers;
+  headers["Content-Type"] = "text/html";
+  headers["Content-Length"] = "0";
+  headers["Connection"] = "close";
+
+  // Return response
   HttpResponse response;
   response.code = 200;
-  response.body = "This is an example response";
+  response.codeDescription = "OK";
+  response.headers = headers;
   return response;
 }
